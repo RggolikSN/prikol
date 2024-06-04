5c812203392fb25b27dad5c784446f35a09964e9
RGGOLIK <152860170+RggolikSN@users.noreply.github.com>
2024-06-04 15:13:35 +0300

    Add files via upload

---
+++ clicker/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const clickButton = document.getElementById('clickbutton');
+    const clickedText = document.getElementById('clicked');
+
+    // Получаем сохраненное значение из локального хранилища (если оно есть)
+    let count = localStorage.getItem('clickCount') || 0;
+
+    // Обновляем текст на странице с сохраненным значением
+    clickedText.textContent = count;
+
+    function incrementCount() {
+        // Увеличиваем счетчик
+        count++;
+        // Обновляем текст на странице
+        clickedText.textContent = count;
+        // Сохраняем значение в локальное хранилище
+        localStorage.setItem('clickCount', count);
+    }
+
+    clickButton.addEventListener('click', incrementCount);
+});
